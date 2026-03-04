81bbaecd924d1185f933861e3c705c9fcbedd77b
Athrav-0515 <patilatharv0515@gmail.com>
2026-03-04 06:51:45 -0800

    ADD NEW informatioon

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="app">
       <h1>Hello Athrav</h1>
       <p>Welcome to your simple landing page.</p>
+      <p>This is a simple paragraph for demonstration purposes.</p>
     </div>
   );
 }
